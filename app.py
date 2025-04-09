15a9024c1d3f7b531ed673d30bfc0d4c3028500a
QwertyQarrot <modernmohammed@hotmail.co.uk>
2025-04-09 17:06:13 +0100

    Change

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World from Flask!"
+    return "Akrima is a big faggot"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
